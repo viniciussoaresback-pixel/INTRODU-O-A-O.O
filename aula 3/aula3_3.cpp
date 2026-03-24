db84a4d6a13ec10507b030810f239f5c0540b02a
viniciussoaresback-pixel <viniciussoaresback@gmail.com>
2026-03-24 19:35:43 -0300

    Add files via upload

---
+++ aula 3/aula3_3.cpp
@@ -0,0 +1,19 @@
+#include <iostream> //biblioteca principal do c++
+using namespace std;
+
+int main() 
+{
+    float num1, num2;
+    //recebendo os dados
+    cout << "Digite o primeiro numero: ";
+    cin >> num1;
+
+
+
+
+    cout << "Digite o segundo numero: ";
+    cin >> num2;
+
+    cout << "A soma dos numeros e: " << num1 + num2;
+ return 0;
+}
