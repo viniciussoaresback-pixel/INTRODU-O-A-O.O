69b6d5b77407e06110220e011182c0baa17bffa8
viniciussoaresback-pixel <viniciussoaresback@gmail.com>
2026-03-26 21:34:28 -0300

    Add files via upload

---
+++ aula 5/aula5_4.cpp
@@ -0,0 +1,20 @@
+// aula 5 exercício 3
+#include <iostream> 
+using namespace std;
+int main()
+{
+    bool porta;
+    cout << "digite se a porta esta aberta (1 sim 0 nao)";
+    cin >> porta;
+    if (porta)
+    {
+        cout << "a porta está aberta favor fechar e ligar o
+                    ar -
+                    condicionado"; }
+                    else 
+         {
+            cout << "ligar ar-condicionado";
+         }
+         return 0;  
+    }
+    
