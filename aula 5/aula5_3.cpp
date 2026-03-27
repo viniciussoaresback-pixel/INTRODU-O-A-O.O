3f2c0e20e228180697b9ac5beda07c5ae9d31571
viniciussoaresback-pixel <viniciussoaresback@gmail.com>
2026-03-26 21:32:53 -0300

    Add files via upload

--- aula 5/aula5_3.cpp
+++ aula 5/aula5_3.cpp
@@ -1,20 +1,49 @@
-// aula 5 exercício 3
-#include <iostream> 
+// aula 5 exercicio 4 
+#include <iostream>
 using namespace std;
-int main()
-{
-    bool porta;
-    cout << "digite se a porta esta aberta (1 sim 0 nao)";
-    cin >> porta;
-    if (porta)
-    {
-        cout << "a porta está aberta favor fechar e ligar o
-                    ar -
-                    condicionado"; }
-                    else 
-         {
-            cout << "ligar ar-condicionado";
-         }
-         return 0;  
-    }
+int main() {
+    float n1=0, n2=0, media=0;
+    int faltas=0, idade=0;
+    bool aprova, emancipado;
+    cout << "Digite qtd faltas: ";
+    cin >> faltas;
+    cout << "Digite a Nota 1: ";
+    cin >> n1;
+    cout << "Digite a Nota 2: ";
+    cin >> n2;
+    media = (n1 + n2)/ 2;
+    // if obrigatorio parenteses
+    if ((faltas < 180) && (media>=6)) { //usando AND
+        // || or - != not equal ou diferente
+        cout << "aprovado"; }
+    else {
+        if (faltas >=180) {
+            cout << "reprovado";
+        }
+        else {
+            cout << "aprova conselho (0 nao 1 sim)";
+            cin >> aprova;
+            if (!(aprova)) { // usando DIFERENTE ou NOT negando o valor
+                cout << "reprovado por conselho de classe";}
+            else {
+                cout <<"Alubo aprovado";
+            }   }
+        }
     
+        cout << "digite a idade";
+        cin >> idade;
+        cout << "digite se for emancipado(1 sim 0 nao)";
+        cin >> emancipado;
+
+        if ((idade >= 18) || (emancipado)) {
+            cout << "voce pode retirar o boletim";
+        }
+        else {
+            cout << "os responsaveis devem retirar o boletim";}
+
+        if (idade >= 18) {
+            cout << "a partir deste mes o boleto segue em seu nome...";
+        }
+        return 0;
+}
+
