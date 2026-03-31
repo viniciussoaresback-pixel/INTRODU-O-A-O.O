de6c35cdb14a81e582aaa9aec9f4d9c51a6a13d4
viniciussoaresback-pixel <viniciussoaresback@gmail.com>
2026-03-31 20:08:30 -0300

    Add files via upload

---
+++ aula 6/aula6_3.cpp
@@ -0,0 +1,15 @@
+// aula 6 exercicio 3 
+#include <iostream> // exemplo while
+using namespace std;
+
+int main()
+{
+    int contador=1;
+
+    while(contador<=10){
+        cout<< contador << endl;
+        contador++;
+    }
+
+    return 0;
+}
