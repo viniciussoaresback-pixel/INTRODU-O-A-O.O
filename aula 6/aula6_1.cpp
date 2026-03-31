9909dd91492906fb7d260c938ea06c7694485d01
viniciussoaresback-pixel <viniciussoaresback@gmail.com>
2026-03-31 19:53:52 -0300

    Add files via upload

---
+++ aula 6/aula6_1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Loop para imprimir de 1 até 5 
+    for (int i = 1; i <= 5; i++) {
+        cout << "Número: "  << i << endl;
+    }
+    return 0;
+}
