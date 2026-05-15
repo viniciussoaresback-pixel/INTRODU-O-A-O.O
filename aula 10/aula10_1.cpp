8f9b3c057ca658d0063280460cbdb4e1a902d664
viniciussoaresback-pixel <viniciussoaresback@gmail.com>
2026-05-14 21:48:34 -0300

    Update aula10_1.cpp

--- aula 10/aula10_1.cpp
+++ aula 10/aula10_1.cpp
@@ -17,3 +17,32 @@ public:
         saldo = saldoInicial;
      }
 } 
+
+ // aula 14/05
+ // Depositar dinheiro metodo
+ void depositar (double valor) {
+   saldo += valor;
+   cout << "Deposito realizado com sucesso!" << endl;
+ }
+
+ // Sacar dinheiro metodo
+   void sacar(double valor) {
+     if (valor <= saldo) {
+        saldo -= valor;
+        cout << "Saque realizado com sucesso!" << endl;
+    } else {
+        cout <, " Saldo insuficiente" << endl;
+    }
+}
+ 
+// Exibir dados da conta 
+    void exibirDados() {
+    cout << "\n=== DADOS DA CONTA ===" << endl;
+    cout << "Titular: " << titular << endl;
+    cout << "Conta: " << numeroConta << endl;
+    cout << "Saldo: R$ " << saldo << endl;
+    }
+};
+
+
+ 
